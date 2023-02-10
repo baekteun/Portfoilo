7e6c02bb503e81ae0b5659145e91746f4bc3fb50
baegteun <baegteun@gmail.com>
2023-02-10 21:11:02 +0900

    :sparkles: :: 앱 리스트

--- Sources/App/ViewControllers/Portfoilo/Entity/AppItem.swift
+++ Sources/App/ViewControllers/Portfoilo/Entity/AppItem.swift
@@ -6,9 +6,14 @@ struct AppItem: Equatable, Hashable {
 }
 
 let appItems: [AppItem] = [
+    .init(name: "GUI", imgPath: "images/app/gui.svg"),
+    .init(name: "심통 - Simtong", imgPath: "images/app/simtong.png"),
+    .init(name: "DMS", imgPath: "images/app/dms.png"),
+    .init(name: "오늘뭐임", imgPath: "images/app/todaywhat.png"),
+    .init(name: "GRIG", imgPath: "images/app/grig.png"),
+    .init(name: "모이자!", imgPath: "images/app/moiza.png"),
     .init(name: "GCMS", imgPath: "images/app/gcms.png"),
-    .init(name: "GCMS", imgPath: "images/app/gcms.png"),
-    .init(name: "GCMS", imgPath: "images/app/gcms.png"),
-    .init(name: "GCMS", imgPath: "images/app/gcms.png")
+    .init(name: "GAuth", imgPath: "images/app/gauth.svg"),
+    .init(name: "그저 메모", imgPath: "images/app/justmemo.png")
 ]
 

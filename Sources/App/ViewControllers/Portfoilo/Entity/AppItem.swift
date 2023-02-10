53d0c6d6a52194023173bc6612dff9abd3db6b11
baegteun <baegteun@gmail.com>
2023-02-10 21:12:14 +0900

    :sparkles: :: 앱들 ID 지정

--- Sources/App/ViewControllers/Portfoilo/Entity/AppItem.swift
+++ Sources/App/ViewControllers/Portfoilo/Entity/AppItem.swift
@@ -1,19 +1,20 @@
 import Web
 
 struct AppItem: Equatable, Hashable {
+    let id: String
     let name: String
     let imgPath: String
 }
 
 let appItems: [AppItem] = [
-    .init(name: "GUI", imgPath: "images/app/gui.svg"),
-    .init(name: "심통 - Simtong", imgPath: "images/app/simtong.png"),
-    .init(name: "DMS", imgPath: "images/app/dms.png"),
-    .init(name: "오늘뭐임", imgPath: "images/app/todaywhat.png"),
-    .init(name: "GRIG", imgPath: "images/app/grig.png"),
-    .init(name: "모이자!", imgPath: "images/app/moiza.png"),
-    .init(name: "GCMS", imgPath: "images/app/gcms.png"),
-    .init(name: "GAuth", imgPath: "images/app/gauth.svg"),
-    .init(name: "그저 메모", imgPath: "images/app/justmemo.png")
+    .init(id: "GUI", name: "GUI", imgPath: "images/app/gui.svg"),
+    .init(id: "SIMTONG", name: "심통 - Simtong", imgPath: "images/app/simtong.png"),
+    .init(id: "DMS", name: "DMS", imgPath: "images/app/dms.png"),
+    .init(id: "TODAYWHAT", name: "오늘뭐임", imgPath: "images/app/todaywhat.png"),
+    .init(id: "GRIG", name: "GRIG", imgPath: "images/app/grig.png"),
+    .init(id: "MOIZA", name: "모이자!", imgPath: "images/app/moiza.png"),
+    .init(id: "GCMS", name: "GCMS", imgPath: "images/app/gcms.png"),
+    .init(id: "GAUTH", name: "GAuth", imgPath: "images/app/gauth.svg"),
+    .init(id: "JUSTMEMO", name: "그저 메모", imgPath: "images/app/justmemo.png")
 ]
 

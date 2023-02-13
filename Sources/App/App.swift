9d627bc6904421e2849b1d25957f9aac236dbc9c
baegteun <baegteun@gmail.com>
2023-02-13 10:36:36 +0900

    :fire: :: not found page

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -3,8 +3,7 @@ import Web
 public class App: WebApp {
     @AppBuilder public override var app: AppBuilder.Content {
         Routes {
-            Page("**") { NotFoundViewController() }
-            Page("") { PortfoiloViewController() }
+            Page("**") { PortfoiloViewController() }
         }
     }
 }

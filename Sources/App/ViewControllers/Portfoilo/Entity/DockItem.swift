3ba2700ab14fad1723e7709c7ff8a17f59a1a0d2
baegteun <baegteun@gmail.com>
2023-02-13 21:51:07 +0900

    :bento: :: call image to discord

--- Sources/App/ViewControllers/Portfoilo/Entity/DockItem.swift
+++ Sources/App/ViewControllers/Portfoilo/Entity/DockItem.swift
@@ -6,7 +6,7 @@ struct DockItem: Equatable, Hashable {
 }
 
 let dockItems: [DockItem] = [
-    .init(imgPath: "images/dock/call.png", dynamicIslandDisplay: .telephone),
+    .init(imgPath: "images/dock/discord.jpg", dynamicIslandDisplay: .discord),
     .init(imgPath: "images/dock/mail.png", dynamicIslandDisplay: .mail),
     .init(imgPath: "images/dock/appstore.png", dynamicIslandDisplay: .appStore),
     .init(imgPath: "images/dock/github.png", dynamicIslandDisplay: .github),
